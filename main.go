520aa2df72dd9c2247d3b504bf4c9f2fe017b67d
Melker Stålnacke <melkerstalnacke@gmail.com>
2025-01-31 08:13:31 +0100

    erm soon test

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"math"
 	"sort"
+	"time"
 
 	"github.com/BurntSushi/toml"
 	govanza "github.com/Melle101/govanza"
@@ -16,6 +18,7 @@ type position struct {
 	orderbookID  string
 	value        float64
 	underlyingID string
+	volume       float64
 }
 
 type settings struct {
@@ -43,9 +46,11 @@ type assetInfo struct {
 	relativeSMA      float64
 }
 type tradeInfo struct {
-	sellAsset string
-	buyAsset  string
-	maxValue  float64
+	sellAsset    string
+	buyAsset     string
+	sellValue    float64
+	extraBuyCash float64
+	volume       float64
 }
 
 func getPercentageChange(assetID string, period string) float32 {
@@ -113,13 +118,13 @@ func findUpcomingHoldings(config config) []string {
 	return upcomingHoldings
 }
 
-func contains(s []string, e string) bool {
-	for _, a := range s {
+func containsStr(s []string, e string) (bool, int) {
+	for i, a := range s {
 		if a == e {
-			return true
+			return true, i
 		}
 	}
-	return false
+	return false, -1
 }
 
 func indexOfAsset(s []asset, e string) int {
@@ -131,9 +136,19 @@ func indexOfAsset(s []asset, e string) int {
 	return -1
 }
 
-func indexOfPos(s []position, e string) int {
+func indexOfPosOrderID(s []position, e string) int {
+
 	for i, pos := range s {
 		if pos.orderbookID == e {
+			return i
+		}
+	}
+	return -1
+}
+func indexOfPosUnderlyingID(s []position, e string) int {
+
+	for i, pos := range s {
+		if pos.underlyingID == e {
 			return i
 		}
 	}
@@ -144,11 +159,10 @@ func findSuitableAsset(underlying string, targetLev int) string {
 
 	searchInfo := constants.WarrantSearch{
 		Filter: constants.Filter{
-			Directions:            []string{"long"},
-			Issuers:               []string{},
-			SubTypes:              []string{"mini_future"},
-			EndDates:              []string{},
-			UnderlyingInstruments: []string{underlying},
+			Directions: []string{"long"},
+			Issuers:    []string{},
+			SubTypes:   []string{"mini_future"},
+			EndDates:   []string{},
 		},
 		Limit:  20,
 		Offset: 0,
@@ -156,6 +170,12 @@ func findSuitableAsset(underlying string, targetLev int) string {
 			Field: "leverage",
 			Order: "asc",
 		},
+	}
+
+	if underlying == "155458" {
+		searchInfo.Filter.NameQuery = "SP500"
+	} else {
+		searchInfo.Filter.UnderlyingInstruments = []string{underlying}
 	}
 
 	warrantList, _ := govanza.GetWarrantList(searchInfo)
@@ -188,45 +208,267 @@ func removeDuplicateStr(strSlice []string) []string {
 	return list
 }
 
-// There will be a bug if currentPositions < upcomingHoldings. eg. if double in backup asset.
-func findTrades(currentPositions []position, upcomingHoldings []string, config config) []tradeInfo {
+func normalizePositions(positions []position, config config) ([]position, error) {
+	if len(positions) == config.Settings.AGG {
+		return positions, nil
+	}
+
+	backupIndex := -1
+
+	for i, pos := range positions {
+		if pos.orderbookID == config.Settings.BackupAsset {
+			backupIndex = i
+		}
+	}
+
+	if backupIndex == -1 {
+		return positions, errors.New("Couldn't normalize positions, backup asset not found in positions")
+	}
+
+	missingPositions := config.Settings.AGG - len(positions)
+
+	positions[backupIndex].value /= (float64(missingPositions) + 1)
+
+	for range missingPositions {
+		positions = append(positions, positions[backupIndex])
+	}
+
+	return positions, nil
+}
+
+func findTrades(currentPositions []position, upcomingHoldings []string, config config) ([]tradeInfo, error) {
+
+	var trades []tradeInfo
+
+	normalizedPositions, err := normalizePositions(currentPositions, config)
+
+	if err != nil {
+		return trades, err
+	}
 
 	var sells []string
 	var buys []string
 
+	fmt.Println(normalizedPositions)
+
 	for _, next := range upcomingHoldings {
-		contain := false
 
-		for _, pos := range currentPositions {
-			if !contains(upcomingHoldings, pos.underlyingID) {
-				sells = append(sells, pos.orderbookID)
-			}
-
-			if next == pos.underlyingID {
-				contain = true
-			}
-		}
-		if !contain {
+		if indexOfPosUnderlyingID(normalizedPositions, next) == -1 {
 			buys = append(buys, next)
 		}
 	}
-	nsells := removeDuplicateStr(sells)
 
-	trades := make([]tradeInfo, len(nsells))
-
-	for i := range nsells {
-		assetIndex := indexOfAsset(config.Universe, buys[i])
-		buyAsset := findSuitableAsset(buys[i], config.Universe[assetIndex].TargetLev)
-		sellIndex := indexOfPos(currentPositions, nsells[i])
-
-		trades[i] = tradeInfo{
-			sellAsset: nsells[i],
-			buyAsset:  buyAsset,
-			maxValue:  currentPositions[sellIndex].value,
+	for _, pos := range normalizedPositions {
+		contains, index := containsStr(upcomingHoldings, pos.underlyingID)
+		if !contains {
+			sells = append(sells, pos.orderbookID)
+		} else {
+			upcomingHoldings[index] = ""
 		}
 	}
 
-	return trades
+	trades = make([]tradeInfo, len(sells))
+	fmt.Println(sells)
+	fmt.Println(buys)
+
+	for i := range sells {
+		var buyAsset string
+
+		if buys[i] == config.Settings.BackupAsset {
+			buyAsset = buys[i]
+		} else {
+			assetIndex := indexOfAsset(config.Universe, buys[i])
+			buyAsset = findSuitableAsset(buys[i], config.Universe[assetIndex].TargetLev)
+		}
+		sellIndex := indexOfPosOrderID(normalizedPositions, sells[i])
+
+		trades[i] = tradeInfo{
+			sellAsset: sells[i],
+			buyAsset:  buyAsset,
+			sellValue: normalizedPositions[sellIndex].value,
+			volume:    normalizedPositions[sellIndex].volume,
+		}
+	}
+
+	return trades, nil
+}
+
+func fundToWarrant(client govanza.Client, accID string, tradeInfo tradeInfo) (string, error) {
+	respSell, onAccountDate, err := placeFundOrderWithReTries(client, accID, "SELL", tradeInfo.sellAsset, tradeInfo.sellValue, tradeInfo.volume)
+
+	nextTradeTime := time.Date(onAccountDate.Year(), onAccountDate.Month(), onAccountDate.Day(), 11, 0, 0, 0, time.Local)
+	duration := time.Until(nextTradeTime)
+
+	if duration > 0 {
+		time.Sleep(duration)
+	}
+
+	client.Auth()
+	sellStatus, err := client.GetOrderStatus(accID, respSell.OrderID)
+	client.Disconnect()
+
+	if sellStatus.Status != "FULLY_EXECUTED" || err != nil {
+		return "", errors.New("Problem with executing sell of asset: " + tradeInfo.sellAsset)
+	}
+
+	buyValue := tradeInfo.sellValue + tradeInfo.extraBuyCash
+
+	respBuy, err := placeOrderWithReTries(client, accID, "BUY", tradeInfo.buyAsset, buyValue, 0.00)
+	if err != nil {
+		return "", err
+	}
+
+	client.Auth()
+	buyStatus, err := client.GetOrderStatus(accID, respSell.OrderID)
+	client.Disconnect()
+
+	if buyStatus.Status != "FULLY_EXECUTED" || err != nil {
+		return "", errors.New("Problem with executing buy of asset: " + tradeInfo.buyAsset)
+	}
+
+	return respBuy.OrderID, nil
+}
+
+func warrantToWarrant(client govanza.Client, accID string, tradeInfo tradeInfo) (string, error) {
+	respSell, err := placeOrderWithReTries(client, accID, "SELL", tradeInfo.sellAsset, tradeInfo.sellValue, tradeInfo.volume)
+	if err != nil {
+		return "", err
+	}
+
+	client.Auth()
+	sellStatus, err := client.GetOrderStatus(accID, respSell.OrderID)
+	client.Disconnect()
+
+	if sellStatus.Status != "FULLY_EXECUTED" || err != nil {
+		return "", errors.New("Problem with executing sell of asset: " + tradeInfo.sellAsset)
+	}
+
+	buyValue := tradeInfo.sellValue + tradeInfo.extraBuyCash
+
+	respBuy, err := placeOrderWithReTries(client, accID, "BUY", tradeInfo.buyAsset, buyValue, 0.00)
+	if err != nil {
+		return "", err
+	}
+
+	client.Auth()
+	buyStatus, err := client.GetOrderStatus(accID, respSell.OrderID)
+	client.Disconnect()
+
+	if buyStatus.Status != "FULLY_EXECUTED" || err != nil {
+		return "", errors.New("Problem with executing buy of asset: " + tradeInfo.buyAsset)
+	}
+
+	return respBuy.OrderID, nil
+
+}
+
+func warrantToFund(client govanza.Client, accID string, tradeInfo tradeInfo) (string, error) {
+	respSell, err := placeOrderWithReTries(client, accID, "SELL", tradeInfo.sellAsset, tradeInfo.sellValue, tradeInfo.volume)
+	if err != nil {
+		return "", err
+	}
+
+	client.Auth()
+	sellStatus, err := client.GetOrderStatus(accID, respSell.OrderID)
+	client.Disconnect()
+
+	if sellStatus.Status != "FULLY_EXECUTED" || err != nil {
+		return "", errors.New("Problem with executing sell of asset: " + tradeInfo.sellAsset)
+	}
+
+	buyValue := tradeInfo.sellValue + tradeInfo.extraBuyCash
+
+	respBuy, onAccountDate, err := placeFundOrderWithReTries(client, accID, "BUY", tradeInfo.buyAsset, buyValue, 0.00)
+	if err != nil {
+		return "", err
+	}
+
+	tradeDoneTime := time.Date(onAccountDate.Year(), onAccountDate.Month(), onAccountDate.Day(), 11, 0, 0, 0, time.Local)
+	duration := time.Until(tradeDoneTime)
+
+	if duration > 0 {
+		time.Sleep(duration)
+	}
+
+	client.Auth()
+	buyStatus, err := client.GetOrderStatus(accID, respBuy.OrderID)
+	client.Disconnect()
+
+	if buyStatus.Status != "FULLY_EXECUTED" || err != nil {
+		return "", errors.New("Problem with executing buy of asset: " + tradeInfo.buyAsset)
+	}
+
+	return respBuy.OrderID, nil
+
+}
+
+func placeFundOrderWithReTries(client govanza.Client, accID, side, asset string, value, volume float64) (constants.FundOrderResponse, time.Time, error) {
+	client.Auth()
+
+	var onAccountDate time.Time
+	var resp constants.FundOrderResponse
+
+	assetInfo, _ := govanza.GetPriceInfo(asset)
+
+	if volume == 0.00 {
+		volume = math.Floor(value / assetInfo[0].LastPrice)
+	}
+
+	for i := range 10 {
+		resp, onAccountDate, _ = client.PlaceFundOrder(accID, asset, side, assetInfo[0].LastPrice, volume)
+
+		if resp.OrderRequestStatus == "SUCCESS" {
+			break
+		} else if i == 10 {
+			return resp, onAccountDate, errors.New("Could not place position. AssetID: " + asset)
+		} else {
+			time.Sleep(10 * time.Second)
+		}
+	}
+
+	client.Disconnect()
+	return resp, onAccountDate, nil
+}
+
+func placeOrderWithReTries(client govanza.Client, accID, side, asset string, value, volume float64) (constants.PlaceOrder, error) {
+	client.Auth()
+
+	assetInfo, _ := govanza.GetPriceInfo(asset)
+
+	if volume == 0.00 {
+		volume = math.Floor(value / assetInfo[0].LastPrice)
+	}
+
+	var resp constants.PlaceOrder
+
+	for i := range 10 {
+		resp, _ = client.PlaceOrder(accID, asset, side, int(volume), assetInfo[0].LastPrice)
+
+		if resp.OrderRequestStatus == "SUCCESS" {
+			break
+		} else if i == 10 {
+			return resp, errors.New("Could not place position. AssetID: " + asset)
+		} else {
+			time.Sleep(10 * time.Second)
+		}
+	}
+
+	client.Disconnect()
+
+	return resp, nil
+}
+
+func makeTrade(client govanza.Client, accID string, tradeInfo tradeInfo, config config) {
+
+	if tradeInfo.sellAsset == config.Settings.BackupAsset {
+		fundToWarrant(client, accID, tradeInfo)
+
+	} else if tradeInfo.buyAsset == config.Settings.BackupAsset {
+		warrantToFund(client, accID, tradeInfo)
+	} else {
+		warrantToWarrant(client, accID, tradeInfo)
+	}
+
 }
 
 func main() {
@@ -254,28 +496,68 @@ func main() {
 		log.Println(err)
 	}
 
+	accounts, _ := client.GetAccountsRAW()
+	var accID string
+
+	for _, acc := range accounts {
+		if acc.URLParameterID == ACC_URL {
+			accID = acc.ID
+		}
+	}
+
+	client.Disconnect()
+
 	currentPositions := make([]position, len(positions.AssetPositions))
 
 	for i, pos := range positions.AssetPositions {
 
-		if pos.ID == config.Settings.BackupAsset {
-			currentPositions[i].underlyingID = config.Settings.BackupAsset
+		if pos.OrderbookID == config.Settings.BackupAsset {
+			currentPositions[i] = position{
+				underlyingID: config.Settings.BackupAsset,
+				orderbookID:  config.Settings.BackupAsset,
+				value:        pos.TotalValue,
+				volume:       pos.Shares,
+			}
+
 		} else {
 			warrantInfo, _ := govanza.GetWarrantInfo(pos.OrderbookID)
-			currentPositions[i].underlyingID = warrantInfo.Underlying.OrderbookID
-		}
 
-		currentPositions[i].orderbookID = pos.OrderbookID
-		currentPositions[i].value = pos.TotalValue
+			currentPositions[i] = position{
+				underlyingID: warrantInfo.Underlying.OrderbookID,
+				orderbookID:  pos.OrderbookID,
+				value:        pos.TotalValue,
+				volume:       pos.Shares,
+			}
+		}
 	}
 
 	upcomingHoldings := findUpcomingHoldings(config)
+	fmt.Println(currentPositions)
 	fmt.Println("Bör hålla dessa underliggande nästa period.")
 	fmt.Println(upcomingHoldings)
 
-	trades := findTrades(currentPositions, upcomingHoldings, config)
+	trades, err := findTrades(currentPositions, upcomingHoldings, config)
+
+	totalCash := 0.0
+	for _, cash := range positions.CashPositions {
+		totalCash += cash.TotalValue
+	}
+
+	for _, trade := range trades {
+		trade.extraBuyCash += (totalCash / float64(len(trades))) * 0.95
+	}
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	fmt.Println("Gör detta bytet mellan de två Mini_Futures.")
 	fmt.Println(trades)
 
-	//client.Disconnect()
+	for _, trade := range trades {
+		go makeTrade(client, accID, trade, config)
+	}
+
+	s, _ := client.Disconnect()
+	fmt.Println(s)
 }
